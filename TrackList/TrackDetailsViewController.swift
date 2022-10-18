8ac42fa75f6f931b6aa3c4c67cc7ece1d87e8bbc
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2022-10-18 07:25:03 +0300

    implemented the operation of the TrackDetailsViewController

--- TrackList/TrackDetailsViewController.swift
+++ TrackList/TrackDetailsViewController.swift
@@ -8,11 +8,15 @@
 import UIKit
 
 class TrackDetailsViewController: UIViewController {
-    
     var track: Track!
+    
+    @IBOutlet var artCoverImageView: UIImageView!
+    @IBOutlet var trackTitleLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        artCoverImageView.image = UIImage(named: track.title)
+        trackTitleLabel.text = track.title
     }
 
 }
